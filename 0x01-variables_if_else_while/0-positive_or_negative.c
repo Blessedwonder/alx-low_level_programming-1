3f14ddfa89d578589b7301558d92cb87174f708d
Iyanda Abubakar <iyandaabubakar@gmail.com>
2022-07-01 11:45:27 -0700

    my ifelse

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,25 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/**
- *main -> assign a random number to the variable n each time it is executed.
- *Return : always 0
+/** 
+*main -> assign a random number to the variable n when executed
+*based on a condition
+*Return : always zero
 */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	       printf("%d is positive\n",n);
-        if (n == 0)
-	       printf("%d is zero\n" ,n); 
-	if (n < 0){ 
-               printf("%d is negative\n" 'n);
-}	
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%d is positive\n", n);
+if (n == 0)
+	printf("%d is zero\n", n);
+if (n < 0)
+{
+	printf("%d is negative\n", n);
 }
-
-
-
-
+return (0);
+}
