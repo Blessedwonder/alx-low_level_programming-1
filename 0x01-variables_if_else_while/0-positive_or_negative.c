ccec338fa624ec29586b38f75e716cad3ab8ad37
Iyanda Abubakar <iyandaabubakar@gmail.com>
2022-07-01 12:29:09 -0700

    my betty elseif

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/** 
-*main -> assign a random number to the variable n when executed
-*based on a condition
-*Return : always zero
-*/
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
